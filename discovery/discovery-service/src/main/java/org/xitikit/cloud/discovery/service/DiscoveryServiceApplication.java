9700646f397104ceb82ce480a0f35b68231dd08a
jhoopes2 <jonathan.hoopes@optum.com>
2018-07-27 15:56:51 -0700

    Adding config server config

--- discovery/discovery-service/src/main/java/org/xitikit/cloud/discovery/service/DiscoveryServiceApplication.java
+++ discovery/discovery-service/src/main/java/org/xitikit/cloud/discovery/service/DiscoveryServiceApplication.java
@@ -2,9 +2,10 @@ package org.xitikit.cloud.discovery.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+@EnableConfigurationProperties
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
