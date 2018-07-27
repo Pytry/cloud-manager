e5c686e9fd9adca9d3e4c9de97a454c95b0ece45
jhoopes2 <jonathan.hoopes@optum.com>
2018-07-27 15:54:47 -0700

    Updates for config server repo

---
+++ discovery/discovery-service/src/main/java/org/xitikit/cloud/discovery/service/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package org.xitikit.cloud.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
