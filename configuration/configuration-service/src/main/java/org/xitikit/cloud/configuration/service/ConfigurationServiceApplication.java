583b121fb96b52491cb255b5ccce8a30eaa21fe9
jhoopes2 <jonathan.hoopes@optum.com>
2018-07-27 13:50:44 -0700

    Adding config server and a bunch of basic files.

---
+++ configuration/configuration-service/src/main/java/org/xitikit/cloud/configuration/service/ConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.xitikit.cloud.configuration.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+}
