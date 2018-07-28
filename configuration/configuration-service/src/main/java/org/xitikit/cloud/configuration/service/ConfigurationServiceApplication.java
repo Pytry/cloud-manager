044d5440afe16d23c311adc7afb494b976622eb9
jhoopes2 <jonathan.hoopes@optum.com>
2018-07-27 17:36:30 -0700

    Setting up fortune and greeting

--- configuration/configuration-service/src/main/java/org/xitikit/cloud/configuration/service/ConfigurationServiceApplication.java
+++ configuration/configuration-service/src/main/java/org/xitikit/cloud/configuration/service/ConfigurationServiceApplication.java
@@ -2,7 +2,13 @@ package org.xitikit.cloud.configuration.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigurationProperties
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigurationServiceApplication {
 
--- discovery/discovery-service/src/main/java/org/xitikit/cloud/discovery/service/DiscoveryServiceApplication.java
+++ discovery/discovery-service/src/main/java/org/xitikit/cloud/discovery/service/DiscoveryServiceApplication.java
@@ -3,8 +3,12 @@ package org.xitikit.cloud.discovery.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableConfigurationProperties
+@EnableEurekaServer
+@EnableCircuitBreaker
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
